02816d55235a2fa478a14c3620affa639955a02e
anderson.arendt <anderson.arendt@codeitsolutions.com.br>
2020-04-18 22:19:56 -0300

    Login funcionando

--- src/services/WebhookService.js
+++ src/services/WebhookService.js
@@ -1,5 +1,7 @@
 const fs = require('fs')
 const fetch = require('node-fetch')
+const FormData = require('form-data');
+
 const { findValueElement } = require('../utils/ObjectParser')
 
 const rawData = fs.readFileSync('src/resources/deParaRequest.json');
@@ -13,14 +15,14 @@ async function logarSistema(parameters) {
     cpf = cpf.split(".").join("");
     cpf = cpf.replace('-', '');
 
-    const retornoAPI = await doRequest(action, { senha, cpf })
+    const retornoAPI = await doRequest(action, { senha, cpf }, true)
     const response = {}
 
     if (retornoAPI && !retornoAPI.success) {
-        if (retornoAPI.details['CpfCnpj'].length >= 0) {
+        if (retornoAPI.details['CpfCnpj'] && retornoAPI.details['CpfCnpj'].length >= 0) {
             response.mensagem = 'O CPF informado está inválido. Digite INFORMAR CPF, para informar novamente.'
         }
-        else if (retornoAPI.details['Senha'].length >= 0) {
+        else if (retornoAPI.details['Senha'] && retornoAPI.details['Senha'].length >= 0) {
             response.mensagem = 'Senha informada é inválida, digite INFORMAR SENHA, para informar novamente. Ou digite ESQUECI MINHA SENHA.'
         }
         else {
@@ -36,11 +38,11 @@ async function logarSistema(parameters) {
     return response
 }
 
-async function doRequest(action, parameters) {
+async function doRequest(action, parameters, isFormData) {
     const res = await fetch(`${process.env.API_CONSORCIO_URL}/${action.endpoint}`, {
         method: 'POST',
         headers: buildHeaders(),
-        formData: buildBody(parameters, action.parametros)
+        body: buildBody(parameters, action.parametros, isFormData)
     })
 
     return await res.json()
@@ -53,12 +55,18 @@ function buildHeaders() {
     }
 }
 
-function buildBody(parameters, parametersName) {
+function buildBody(parameters, parametersName, isFormData) {
     const body = {}
 
     parametersName.forEach(p => {
         body[DE_PARA[p]] = parameters[p]
     })
+
+    if(isFormData) {
+        const formData = new FormData();
+        Object.keys(body).forEach(key => formData.append(key, body[key]))
+        return formData
+    }
 
     return body
 }
